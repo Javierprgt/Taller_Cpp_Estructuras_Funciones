f2235ef5b06533e40387e925e8aee005ff02a1da
Javier Pinargote <fpianrgote38@gmail.com>
2026-02-01 00:45:42 -0500

    Update ejercicio1.cpp

--- ejercicios/ejercicio1.cpp
+++ ejercicios/ejercicio1.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int numero;
+    cout << "Ingrese un numero entero: ";
+    cin >> numero;
+
+    if (numero >= 10 && numero <= 50) {
+        cout << "El numero pertenece al rango de 10 a 50." << endl;
+    } else {
+        cout << "El numero NO pertenece al rango de 10 a 50." << endl;
+    }
+
+    return 0;
+}
 
